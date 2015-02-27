90aba33a784d599d92f987a0fd8ca63f0a432ce8
aramdavtyan <aramdavtyan86@gmail.com>
2015-02-27 17:20:05 +0400

    settings

---
+++ web/Справочник/php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/views/css/style.css">
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="/views/js/style.js"></script>
+</head>
+<body>
+	<h1>виртуал хост успешна создан</h1>
+</body>
+</html>
+
+
+
